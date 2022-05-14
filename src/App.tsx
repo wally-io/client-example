0245acadd5c6b14fce160396719ae8bd9fbb60ce
kevin <kevin.draym@gmail.com>
2022-05-15 05:12:32 +0800

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import {Button, Toast} from 'react-bootstrap'
+import './App.scss'
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                Hello world
+            </header>
+        </div>
+    )
+}
+
+export default App
