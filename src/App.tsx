fe58240a4adf24c7f50a4aca676e45cc5dddb3df
kevin <kevin.draym@gmail.com>
2022-05-15 11:37:34 +0800

    fix token

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,13 @@ import Http from "./utils/Http"
 const domain = process.env.REACT_APP_WALLY_API
 
 function App() {
-
-    const [token, setToken] = useState("")
     const [dappRegistrationLink, setDappRegistrationLink] = useState("")
 
     console.log("domain: ", domain)
 
     useEffect(() => {
         Http.post(domain!, "/auth/login", null, {email: "admin@rct.ai", password: "admin123"}, (response: any) => {
-            setToken(response.token)
+            localStorage.setItem("token", response.token)
             Http.get(domain!, "/wallet/dapp/connect/link", response.token, {dappId: "fa94d55a-00fb-41a4-b58a-d50b12092a87", appUserIdentifier: "admin"}, (response: any) => {
                 setDappRegistrationLink(response.path)
             }, (error) => {
