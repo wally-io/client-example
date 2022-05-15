54e342717449a4c1af6839351f9226c3f11bfc07
kevin <kevin.draym@gmail.com>
2022-05-15 11:27:41 +0800

    setup login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import React, {useState} from 'react'
-import {Button, Toast} from 'react-bootstrap'
+import React, {useEffect, useState} from 'react'
 import './App.scss'
+import Http from "./utils/Http"
+
+const domain = process.env.REACT_APP_WALLY_API
 
 function App() {
+
+    const [token, setToken] = useState("")
+    const [dappRegistrationLink, setDappRegistrationLink] = useState("")
+
+    console.log("domain: ", domain)
+
+    useEffect(() => {
+        Http.post(domain!, "/auth/login", null, {email: "admin@rct.ai", password: "admin123"}, (response: any) => {
+            setToken(response.token)
+            Http.get(domain!, "/wallet/dapp/connect/link", response.token, {dappId: "fa94d55a-00fb-41a4-b58a-d50b12092a87", appUserIdentifier: "admin"}, (response: any) => {
+                setDappRegistrationLink(response.path)
+            }, (error) => {
+                console.error(error)
+            })
+        }, (error) => {
+            console.error(error)
+        })
+    }, [])
+
+
     return (
         <div className="App">
             <header className="App-header">
-                Hello world
+                <h1>Hello world</h1>
             </header>
+            <h4>Connect your wallet to DApp: {dappRegistrationLink}</h4>
         </div>
     )
 }
